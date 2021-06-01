cc9b38572e35f3c21c169082d40985c63db36793
Fedor Glukhov <f.glukhov@gmail.com>
2021-06-01 15:00:34 +0400

    UI

--- js/main.js
+++ js/main.js
@@ -209,6 +209,8 @@ $(document).ready(function() {
 
 	// FAQ END
 
+	validateForms();
+
 });
 
 function fixedHeader() {
@@ -254,3 +256,106 @@ function fixedHeader() {
 	};
 
 })(jQuery);
+
+function validateForms() {
+	//$('.textarea-autogrow').autogrow();
+	//$("input.input-phone").mask("+7 (999) 999-99-99");
+	jQuery.validator.addClassRules('phone-email-group', {
+		require_from_group: [1, ".phone-email-group"]
+	});
+	$("select").not(".location-tabs-select").not(".filter-form select").on("change", function() {
+		if (!$(this).closest(".picker").length && !$(this).hasClass("faq-select")) {
+			$(this).valid();
+		}
+	});
+	$("body").on("click", ".form-item", function(e) {
+		if ($(this).find(".bootstrap-select").length && !$(e.target).hasClass("bootstrap-select") && !$(e.target).parents().hasClass("bootstrap-select")) {
+			$(e.target).closest(".form-item").find("select").selectpicker('toggle');
+		}
+	});
+	$("form").each(function() {
+		form = $(this);
+		$(this).validate({
+			focusInvalid: true,
+			sendForm: false,
+			errorPlacement: function(error, element) {
+				if (element[0].tagName == "SELECT") {
+					element.closest(".form-item").addClass("error");
+					element.closest(".btn-group").addClass("btn-group-error");
+					if (element.closest(".form-item").length) {
+						error.insertAfter(element.closest(".form-item"));
+					} else {
+						error.insertAfter(element.closest(".btn-group"));
+					}
+				} else {
+					if (element.attr("type") == "checkbox") {
+						element.siblings("label").addClass("checkbox-label-error")
+					} else {
+						error.insertAfter(element);
+						element.closest(".form-group").addClass("form-group-error");
+					}
+				}
+			},
+			unhighlight: function(element, errorClass, validClass) {
+				$(element).removeClass(errorClass);
+				$(element).closest(".form-item").removeClass("error").addClass("valid");
+				$(element).closest(".form-group").removeClass("form-group-error");
+				if ($(element)[0].tagName == "SELECT") {
+					$(element).closest(".form-item").removeClass("error");
+					$(element).closest(".btn-group").removeClass("btn-group-error");
+					if ($(element).closest(".form-item").length) {
+						error.insertAfter(element.closest(".form-item"));
+						$(element).closest(".form-item").next("label.error").remove();
+					} else {
+						$(element).closest(".btn-group").next("label.error").remove();
+					}
+				} else {
+					$(element).next(".error").remove();
+					if ($(element).attr("type") == "checkbox") {
+						$(element).siblings("label").removeClass("checkbox-label-error")
+					}
+				}
+			},
+			invalidHandler: function(form, validatorcalc) {
+				var errors = validatorcalc.numberOfInvalids();
+				if (errors && validatorcalc.errorList[0].element.tagName == "INPUT") {
+					validatorcalc.errorList[0].element.focus();
+				}
+			},
+			submitHandler: function(form) {
+				$.ajax({
+					type: "POST",
+					data: $(form).serialize(),
+					success: function () {
+						formSuccess(form);
+					}
+				});
+				return false;
+			}
+		});
+		if ($(this).find("input.password").length && $(this).find("input.password-repeat").length) {
+			$(this).find("input.password-repeat").rules('add', {
+				equalTo: "#" + form.find("input.password").attr("id")
+			});
+		}
+	});
+}
+jQuery.extend(jQuery.validator.messages, {
+	required: "Не заполнено поле",
+	remote: "Please fix this field.",
+	email: "Введите правильный e-mail.",
+	url: "Please enter a valid URL.",
+	date: "Please enter a valid date.",
+	dateISO: "Please enter a valid date (ISO).",
+	number: "Please enter a valid number.",
+	digits: "Please enter only digits.",
+	creditcard: "Please enter a valid credit card number.",
+	equalTo: "Пароли не совпадают.",
+	accept: "Please enter a value with a valid extension.",
+	maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+	minlength: jQuery.validator.format("Please enter at least {0} characters."),
+	rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+	range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+	max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+	min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
